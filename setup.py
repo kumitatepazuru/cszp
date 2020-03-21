86cd5e496f4fedbf831a60b99aabb0afab25a840
kumitatepazuru <riku.ueda@outlook.jp>
2020-03-21 19:33:40 +0900

    ver4.1.0 commit

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import glob
+
 from setuptools import setup, find_packages
+
+
+def find(filename):
+    return glob.glob(filename)
+
 
 try:
     with open('README.md') as f:
@@ -14,9 +21,11 @@ def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
 
+cszp_files = ["cszp/index.html", "cszp/nofile.png", "cszp/version"]
+cszp_files += find("cszp/*.json")
 setup(
     name="cszp",
-    version=open("versionlog").read().splitlines()[0],
+    version=open("cszp/version").read().splitlines()[0],
     url='https://github.com/kumitatepazuru/cszp',
     author='kumitatepazuru',
     author_email='teltelboya18@gmail.com',
@@ -25,7 +34,15 @@ setup(
     description='cszp Easy soccer execution program',
     long_description=readme,
     packages=find_packages(),
-    install_requires=_requires_from_file('requirements.txt'),
+    install_requires=[
+        "numpy",
+        "texttable",
+        "matplotlib",
+        "tqdm",
+        "pandas",
+        "urllib3",
+        "cuitools"
+    ],
     license="MIT",
     classifiers=[
         'Programming Language :: Python :: 3 :: Only',
@@ -34,6 +51,7 @@ setup(
     entry_points="""
       # -*- Entry points: -*-
       [console_scripts]
-      cszp = cszp.main:main
-    """
+      cszp = cszp.__init__:main
+    """,
+    data_files=[("cszp", cszp_files), ("cszp/language", find("cszp/language/*.lang"))]
 )
